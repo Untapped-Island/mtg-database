20493bd191bf119a1494e51dcbf92f19b67153e0
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-10-18 19:20:23 +0000

    Add keywords, sets, formats, and color identities

---
+++ db-scripts/create-sets.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client';
+import EnumValues from '../json/EnumValues.json';
+const prisma = new PrismaClient();
+
+const sets = EnumValues.card.watermark.map(watermark => {
+  if (watermark.includes('set (')) {
+    return { id: watermark.substring(5, 8) }
+  }
+})
+
+async function main() {
+  await prisma.type.createMany({
+    data: sets
+  })
+}
+
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  })
